c5ac302e716055ba1196295be9240f649f192f57
JacopoMereu <jachi334@gmail.com>
2024-12-26 18:14:20 +0100

    + Fade quando ci si teletrasporta + Canvas che mostra le log nella speranza di capire qualcosa quando si builda.. + fixato testo che sborda nella scrollview + samples XRI v3 + teleport layer

---
+++ Assets/TeleportAreaWithFade.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using UnityEngine.Events;
+using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.XR.Interaction.Toolkit.Locomotion.Teleportation;
+
+public class TeleportAreaWithFade : TeleportationArea
+{
+    private FadeCanvas fadeCanvas = null;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        fadeCanvas = FadeCanvas.Instance;
+    }
+
+    protected override void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        base.OnSelectEntered(args);
+
+        if (teleportTrigger == TeleportTrigger.OnSelectEntered)
+            StartCoroutine(FadeSequence(base.OnSelectEntered, args));
+    }
+
+    protected override void OnSelectExited(SelectExitEventArgs args)
+    {
+        if (teleportTrigger == TeleportTrigger.OnSelectExited)
+            StartCoroutine(FadeSequence(base.OnSelectExited, args));
+    }
+
+    protected override void OnActivated(ActivateEventArgs args)
+    {
+        if (teleportTrigger == TeleportTrigger.OnActivated)
+            StartCoroutine(FadeSequence(base.OnActivated, args));
+    }
+
+    protected override void OnDeactivated(DeactivateEventArgs args)
+    {
+        if (teleportTrigger == TeleportTrigger.OnDeactivated)
+            StartCoroutine(FadeSequence(base.OnDeactivated, args));
+    }
+
+    private IEnumerator FadeSequence<T>(UnityAction<T> action, T args)
+        where T : BaseInteractionEventArgs
+    {
+        // fadeCanvas.QuickFadeIn(); Original
+        fadeCanvas.StartFadeIn();
+
+        yield return fadeCanvas.CurrentRoutine;
+        action.Invoke(args);
+
+        // fadeCanvas.QuickFadeOut(); Original
+        fadeCanvas.StartFadeOut();
+    }
+}
