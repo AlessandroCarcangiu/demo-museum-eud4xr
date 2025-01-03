a0dc13d9f6e8b18675e8629b08a1f9a59b6f0576
AlessandroCarcangiu <sandro.carcangiu@gmail.com>
2025-01-03 16:53:05 +0100

    aggiornata ECARules4All_DLL.dll + bug fix HomeAssistant_MuseumDemoRules.cs

--- Assets/Scripts/HomeAssistant_MuseumDemoRules.cs
+++ Assets/Scripts/HomeAssistant_MuseumDemoRules.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -13,6 +14,7 @@ using Newtonsoft.Json;
 using Serilog;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Networking;
 using Action = ECARules4All_DLL.Action;
 using Path = ECARules4All_DLL.Path;
 //using System.Text.Json;
@@ -32,12 +34,14 @@ public class HomeAssistant_MuseumDemoRules : MonoBehaviour
     private APIServer _apiServer;
 
     private Settings _settings;
-    //public float moveDistance = 2.0f;
-    //private GameObject objectToMove;
+    
+    private string path;
+
+    private bool isStarted = false;
 
     private void Awake()
     {
-        string path = System.IO.Path.Combine(Application.streamingAssetsPath, "settings.json");
+        string path = System.IO.Path.Combine(Application.streamingAssetsPath, "settings_old.json");
         if (File.Exists(path))
         {
             string json = File.ReadAllText(path);
@@ -48,6 +52,29 @@ public class HomeAssistant_MuseumDemoRules : MonoBehaviour
         {
             Debug.LogError($"Secrets file not found at path: {path}");
         }
+        
+        //string path = System.IO.Path.Combine(Application.streamingAssetsPath, "settings.json");
+        //path = System.IO.Path.Combine(Application.streamingAssetsPath, "settings.json");
+        //StartCoroutine(ReadSettings());
+    }
+
+    IEnumerator ReadSettings()
+    {
+        UnityWebRequest request = UnityWebRequest.Get(path);
+        yield return request.SendWebRequest();
+        
+        if (request.result == UnityWebRequest.Result.ConnectionError ||
+            request.result == UnityWebRequest.Result.ProtocolError)
+        {
+            Debug.LogError("Errore nella lettura del file: " + request.error);
+        }
+        else
+        {
+            string jsonString = request.downloadHandler.text;
+            Debug.Log("File letto correttamente: " + jsonString);
+            _settings = JsonUtility.FromJson<Settings>(jsonString);
+            Debug.Log($"hassUrl: {_settings.hassUrl}, hassToken: {_settings.hassToken}");
+        }
     }
 
 
@@ -56,39 +83,48 @@ public class HomeAssistant_MuseumDemoRules : MonoBehaviour
     { 
         // Home Assistant configuration
         // configure home assistant client
-        AbstractClient<HomeAssistantClient> hassClient = AbstractClient<HomeAssistantClient>.GetInstance();
-        hassClient.url = _settings.hassUrl;
-        hassClient.token = _settings.hassToken;
-        RuleEngine.GetInstance().AddClient(hassClient);
+        /*try
+        {
+            AbstractClient<HomeAssistantClient> hassClient = AbstractClient<HomeAssistantClient>.GetInstance();
+            Debug.Log("1");
+            hassClient.url = "https://fly-powerful-slug.ngrok-free.app";//_settings.hassUrl;
+            Debug.Log("2");
+            hassClient.token =
+                "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI5YjI0M2YzNmY2NTk0MmEyOTM4NGNmODk0MjZhNzQxZCIsImlhdCI6MTcyNTk3NTI2MSwiZXhwIjoyMDQxMzM1MjYxfQ.ylQDAWp0lEs1OGgjxGxAO7LYavuon-FuorspQhM8kDI";//_settings.hassToken;
+            Debug.Log("3");
+            RuleEngine.GetInstance().AddClient(hassClient);
+            Debug.Log("4");
+            
+            // configure api server
+            //_apiServer = new APIServer();
+            //_apiServer = new APIServer("http://0.0.0.0", 5000);//"192.168.1.123");
+            //_apiServer = new APIServer("http://192.168.1.123", 5000);
+            _apiServer = new APIServer("http://+", 5000);
+            Debug.Log("5");
+            _apiServer.ActionUpdate += ((HomeAssistantClient)hassClient).ReceivedUpdateHandler;
+            Debug.Log("6");
+        }
+        catch (Exception e)
+        {
+            Debug.Log($"ERRORE NELLO START DI HOMEASSISTANTDEMORULES {e.Message}");
+        }*/
         
-        // configure api server
-        _apiServer = new APIServer();
-        _apiServer.ActionUpdate += ((HomeAssistantClient)hassClient).ReceivedUpdateHandler;
         // from ngrok terminal digit and execute:
         // ngrok http your_port --host-header="your_url:your_port" -
         // example: ngrok http 8080 --host-header="localhost:8080"
         // from ngrok static url:
-        // ngrok http 8080 --host-header="localhost:8080" --domain="fly-powerful-slug.ngrok-free.app"
-            
+        // ngrok http 8080 --host-header="localhost:8080" --domain="fly-powerful-slug.ngrok-free.app" - unity
+        // ngrok http 8123 --host-header="localhost:8123" --domain="fly-powerful-slug.ngrok-free.app" - hass 
         
-        /*var cube1 = GameObject.Find("Cube1");
-        var cube2 = GameObject.Find("Cube2");
-        Action e = new Action(
-            cube1,
-            "eats",
-            cube2
-        );
-        Action a1 = new Action(
-            cube1,
-            "moves to",
-            new Position(10, 11, 12)
-        );
-        Action a2 = new Action(
-            cube2,
-            "hides"
-        );
-        Rule r = Rule.TryCreateRule(e, new List<Action>() { a1, a2 });
-        RuleEngine.GetInstance().Add(r);*/
+        AbstractClient<HomeAssistantClient> hassClient = AbstractClient<HomeAssistantClient>.GetInstance();
+        hassClient.url = _settings.hassUrl;
+        //hassClient.url = "https://fly-powerful-slug.ngrok-free.app";//
+        //hassClient.token =
+        //    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI5YjI0M2YzNmY2NTk0MmEyOTM4NGNmODk0MjZhNzQxZCIsImlhdCI6MTcyNTk3NTI2MSwiZXhwIjoyMDQxMzM1MjYxfQ.ylQDAWp0lEs1OGgjxGxAO7LYavuon-FuorspQhM8kDI";
+        hassClient.token = _settings.hassToken;
+        RuleEngine.GetInstance().AddClient(hassClient);
+        _apiServer = new APIServer();
+        _apiServer.ActionUpdate += ((HomeAssistantClient)hassClient).ReceivedUpdateHandler;
     }
     
     // Update is called once per frame
@@ -110,7 +146,11 @@ public class HomeAssistant_MuseumDemoRules : MonoBehaviour
     
     private void OnDisable()
     {
-        CloseServer();
+        if (isStarted)
+        {
+            CloseServer();
+            isStarted = false;
+        }
     }
 
     private void OnDestroy()
@@ -125,18 +165,27 @@ public class HomeAssistant_MuseumDemoRules : MonoBehaviour
             _apiServer.Stop();
         }
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+    private void StartServer()
+    {
+        try
+        {
+            AbstractClient<HomeAssistantClient> hassClient = AbstractClient<HomeAssistantClient>.GetInstance();
+            hassClient.url = _settings.hassUrl;
+            //hassClient.url = "https://fly-powerful-slug.ngrok-free.app";//
+            //hassClient.token =
+            //    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI5YjI0M2YzNmY2NTk0MmEyOTM4NGNmODk0MjZhNzQxZCIsImlhdCI6MTcyNTk3NTI2MSwiZXhwIjoyMDQxMzM1MjYxfQ.ylQDAWp0lEs1OGgjxGxAO7LYavuon-FuorspQhM8kDI";
+            hassClient.token = _settings.hassToken;
+            RuleEngine.GetInstance().AddClient(hassClient);
+            
+            _apiServer = new APIServer();
+            //_apiServer = new APIServer("http://+", 5000);
+            _apiServer.ActionUpdate += ((HomeAssistantClient)hassClient).ReceivedUpdateHandler;
+        }
+        catch (Exception e)
+        {
+            Debug.Log($"ERRORE NELLO START DI HOMEASSISTANTDEMORULES {e.Message}");
+        }
+    }
+
 }
