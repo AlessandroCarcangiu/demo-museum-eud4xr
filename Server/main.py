329043dcf5ea483e8de867270aac5a3796f35b04
Diego Serra <serradiego2005@icloud.com>
2026-06-19 18:25:17 +0200

    Add Taverna prefabs, update DLL/docs, add server
    Replace ECARules4All DLL and extend its XML docs (adds beerDrops and ECATankard type/members). Remove old DLL copy under Assets/DLLs. Add ECAObjects prefab folder with ECAFireplace, ECATankard and ECAWindow (and meta files) and update Taverna scene. Update MarkerDetection config and add a small Server folder (main.py, ngrok.exe, marker data and a compiled pyc).

---
+++ Server/main.py
@@ -0,0 +1,23 @@
+﻿from flask import Flask, request
+import base64
+import os
+
+app = Flask(__name__)
+
+PUBLIC_DATA_FOLDER = "public-data"
+os.makedirs(PUBLIC_DATA_FOLDER, exist_ok=True)  # Ensure upload folder exists
+
+@app.route('/get-json-config', methods=['GET'])
+def get_json_config():
+    # Get json data from public-data/config.json
+    config_path = os.path.join(PUBLIC_DATA_FOLDER, 'marker-data-config.json')
+    if not os.path.exists(config_path):
+        return {"error": "Config file not found"}, 404
+    with open(config_path, 'r') as f:
+        config = f.read()
+
+    return config
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8090, debug=True, use_reloader=False)
